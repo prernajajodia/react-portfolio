543c6ca7182fe56fbf67bdca1dd49c6a450237da
Prerna Jajodia <jajodiapj@gmail.com>
2021-02-18 13:15:30 +0530

    email-js integration completed with the form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = ({ data }) => {
    const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
@@ -7,11 +9,19 @@ const Contact = ({ data }) => {
    const [email, setEmail] = useState('');
    const [message, setMessage] = useState('');
 
-   console.log(data)
-
     const handleClick = (e) => {
        e.preventDefault();
-      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+      // window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+
+      emailjs.sendForm('service_65rgilf', 'template_1jt0dmg', e.target, 'user_VRsslVleFz5oO5jsnlWPH')
+      .then((result) => {
+         console.log('Form submit result without error');
+         console.log(result);
+         // window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+         console.log('Form submit result with error');
+          console.log(error);
+      });
     }
     
 
@@ -37,31 +47,31 @@ const Contact = ({ data }) => {
          <div className="row">
             <div className="eight columns">
 
-               <form id="contactForm" name="contactForm">
+               <form id="contactForm" name="contactForm" onSubmit={handleClick}>
 					<fieldset>
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
+						   <input value={name} type="text" size="35" id="contactName" name="from_name" onChange={e => setName(e.target.value)}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
+						   <input value={email} type="text" size="35" id="contactEmail" name="user_email" onChange={e=> setEmail(e.target.value)}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input value={subject} type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
+						   <input value={subject} type="text" size="35" id="contactSubject" name="message_subject" onChange={e => setSubject(e.target.value)}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="message" name="message"></textarea>
                   </div>
 
                   <div>
-                     <button type='submit' onClick={handleClick} className="submit">Submit</button>
+                     <button type='submit' className="submit">Submit</button>
                      <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span>
