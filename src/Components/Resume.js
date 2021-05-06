2be8409c4f226a4dd24a304a7aa632ad9126bda2
Prerna Jajodia <jajodiapj@gmail.com>
2021-05-06 17:42:53 +0530

    Description updated

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -24,7 +24,6 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-
       <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
